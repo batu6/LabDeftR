0ba87d50868d19e29472789291493f2b18ee1bc9
Batuhan Akçabozan <bakcabozan@gmail.com>
2022-12-26 14:47:21 +0100

    LabdeftR - working prototype for experiment overviews!!
    - cleanInput button functions in a separate file.
    
    Coded:
    -File input
    -download
    -manual input
    -update datatable

---
+++ cleanInput.R
@@ -0,0 +1,58 @@
+
+updateTextInput(session, "exp",
+                value = ""
+)
+updateTextInput(session, "expIDM",
+                value = ""
+)
+updateTextInput(session, "mouseM",
+                value = ""
+)
+updateTextInput(session, "inhibitorM",
+                value = ""
+)
+updateTextInput(session, "dilutionM",
+                value = ""
+)
+updateTextInput(session, "stimM",
+                value = ""
+)
+updateTextInput(session, "concM",
+                value = ""
+)
+updateTextInput(session, "roM",
+                value = ""
+)
+updateSelectizeInput(session, "expID",
+                     selected = ""
+)
+updateSelectizeInput(session, "mouse",
+                     selected = ""
+)
+updateSelectizeInput(session, "inhibitor",
+                     selected = ""
+)
+updateSelectizeInput(session, "dilution",
+                     selected = ""
+)
+updateSelectizeInput(session, "stim",
+                     selected = ""
+)
+updateSelectizeInput(session, "conc",
+                     selected = ""
+)
+updateSelectizeInput(session, "ro",
+                     selected = ""
+)
+updateTextAreaInput(session, "purpose",
+                    value = ""
+)
+updateTextAreaInput(session, "design",
+                    value = ""
+)
+updateTextAreaInput(session, "results",
+                    value = ""
+)
+updateTextAreaInput(session, "notes",
+                    value = ""
+)
